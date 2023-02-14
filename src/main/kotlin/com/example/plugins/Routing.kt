51c9015619010c9a1a32c52852f6546b27f8d3ab
Orthodoxal <dimariktron@mail.ru>
2023-02-14 14:39:02 +0400

    Add_photos

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -7,6 +7,7 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.io.File
 
 fun Application.configureRouting() {
     routing {
@@ -17,6 +18,11 @@ fun Application.configureRouting() {
         post("/events") {
             val filterCategoryDto = call.receive(FilterCategoriesReceiveDto::class)
             call.respond(EventRepository().filterEvents(filterCategoryDto))
+        }
+
+        get("/friendPhoto") {
+            val file = File("images/avatar_2.png")
+            call.respondFile(file)
         }
     }
 }
